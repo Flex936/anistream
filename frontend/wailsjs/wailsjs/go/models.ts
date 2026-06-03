a47cc31e0854e29c4332fca80cf6bd505d9b8a15
Tancsik Alex <tancsik.alex@gmail.com>
2026-06-03 14:31:26 +0200

    fix: merge the content of the official docs and this yaml

---
+++ frontend/wailsjs/wailsjs/go/models.ts
@@ -0,0 +1,93 @@
+export namespace main {
+	
+	export class AnimeCover {
+	    large: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new AnimeCover(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.large = source["large"];
+	    }
+	}
+	export class AnimeTitle {
+	    romaji: string;
+	    english: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new AnimeTitle(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.romaji = source["romaji"];
+	        this.english = source["english"];
+	    }
+	}
+	export class Anime {
+	    id: number;
+	    title: AnimeTitle;
+	    coverImage: AnimeCover;
+	    episodes: number;
+	    status: string;
+	    description: string;
+	
+	    static createFrom(source: any = {}) {
+	        return new Anime(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.id = source["id"];
+	        this.title = this.convertValues(source["title"], AnimeTitle);
+	        this.coverImage = this.convertValues(source["coverImage"], AnimeCover);
+	        this.episodes = source["episodes"];
+	        this.status = source["status"];
+	        this.description = source["description"];
+	    }
+	
+		convertValues(a: any, classs: any, asMap: boolean = false): any {
+		    if (!a) {
+		        return a;
+		    }
+		    if (a.slice && a.map) {
+		        return (a as any[]).map(elem => this.convertValues(elem, classs));
+		    } else if ("object" === typeof a) {
+		        if (asMap) {
+		            for (const key of Object.keys(a)) {
+		                a[key] = new classs(a[key]);
+		            }
+		            return a;
+		        }
+		        return new classs(a);
+		    }
+		    return a;
+		}
+	}
+	
+	
+	export class TorrentResult {
+	    title: string;
+	    magnetLink: string;
+	    seeders: string;
+	    size: string;
+	    score: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new TorrentResult(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.title = source["title"];
+	        this.magnetLink = source["magnetLink"];
+	        this.seeders = source["seeders"];
+	        this.size = source["size"];
+	        this.score = source["score"];
+	    }
+	}
+
+}
+
