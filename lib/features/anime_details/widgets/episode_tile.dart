08ceafa4796dee336702715c930e83a3299a4bbe
Mészáros Gergő <gergnoscope@gmail.com>
2026-07-27 12:27:13 +0200

    feat: exoplayer theater screen so it uses surfaceview which is better optimized for TVs

--- lib/features/anime_details/widgets/episode_tile.dart
+++ lib/features/anime_details/widgets/episode_tile.dart
@@ -284,7 +284,11 @@ class EpisodeTile extends StatelessWidget {
                     Navigator.push(
                       context,
                       MaterialPageRoute(
+<<<<<<< HEAD
                         builder: (_) => uiPerformanceMode
+=======
+                        builder: (_) => true
+>>>>>>> 199dd49 (feat: exoplayer theater screen so it uses surfaceview which is better optimized for TVs)
                             ? ExoTheaterScreen(
                                 anime: anime,
                                 episode: episodeNumber,
